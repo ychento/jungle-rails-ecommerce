6a183310b866f0dc3378e575509fdc45cce5faae
ychento <yiyaochen@gmail.com>
2023-10-21 18:28:46 -0400

    add a Categorie page under admin tab

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  namespace :admin do
+    resources :categories
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
